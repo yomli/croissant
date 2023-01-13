9292f31d32a085754c414b4f1c053588169723f6
yomli <yomli@yom.li>
2023-01-13 19:19:07 +0100

    2023 cleanup and writing a Bash building tool

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Croissant
 
-Just another modular CSS framework for personal purpose. I use `_loader.css` to list all the files I need. Then, when everything works, I simply do a `cat file1.css file2.css >> croissant.css` to get a production version. Will write more about all of this later.
+Just another modular CSS framework for personal purpose. I use `_loader.css` to list all the files I need. Then, when everything works, I simply do execute `build.sh` or `cat file1.css file2.css >> croissant.css` to get a production version. Will write more about all of this later.
---
+++ src/build.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -uo pipefail
+
+INPUT="_loader.css"
+# NOW=$(date +"%Y%m%d")
+NOW=$(date +"%s")
+
+while read LINE <&3
+do
+	# Only do this on @import lines
+	if [[ ${LINE:0:7} == "@import" ]]
+	then
+		# Skip @import
+		part1=${LINE##*@import }
+		# Skip url() part
+		part1=${part1##*url(}
+		# Skip '' or "" part
+		part1=${part1##\'}
+		part1=${part1##\"}
+		# Skip the end
+		part2="${part1%%.css*}.css"
+
+		cat $part2 >> croissant-$NOW.css
+		echo $part2 >> croissant-build-$NOW.txt
+	fi
+
+done 3< "$INPUT"
+
+if [[ -f "croissant-$NOW.css" ]]
+then
+	# This command removes comments from CSS and Javascript files using sed.
+	# Courtesy of https://github.com/CMDann/CSSJSMinify
+	cat croissant-$NOW.css | sed -e "s|/\*\(\\\\\)\?\*/|/~\1~/|g" -e "s|/\*[^*]*\*\+\([^/][^*]*\*\+\)*/||g" -e "s|\([^:/]\)//.*$|\1|" -e "s|^//.*$||" | tr '\n' ' ' | sed -e "s|/\*[^*]*\*\+\([^/][^*]*\*\+\)*/||g" -e "s|/\~\(\\\\\)\?\~/|/*\1*/|g" -e "s|\s\+| |g" -e "s| \([{;:,]\)|\1|g" -e "s|\([{;:,]\) |\1|g" > croissant-$NOW.min.css
+fi
+
+exit 0
